80adf3f8b62f8d6c93a7d0b461ddcf0e1a53f955
devansh20la <devansh20la@gmail.com>
2018-03-13 12:59:31 -0400

    Training

--- Classification/Test.py
+++ Classification/Test.py
@@ -7,9 +7,7 @@ from torchvision import datasets,models,transforms
 import os
 import copy
 from data_loader import imageandlabel
-import pdb
-from model import network
-
+import argparse
 
 def model_run(model,inputs,labels,criterion):
         
@@ -25,14 +23,18 @@ def model_run(model,inputs,labels,criterion):
    
     return loss.data[0],corrects,outputs
 
+parser = argparse.ArgumentParser(description='PyTorch Skin Lesion Testing')
+parser.add_argument('--rd','--root_dir',default='home/devansh/Documents/Melanoma/Classification/data/test/')
+parser.add_argument('--cp','--checkpoint',default='model_best.pth.tar')
+args = parser.parse_args()
+
 data_transforms = transforms.Compose([
         transforms.Scale(400),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
-data_dir = '~/Documents/Melanoma/Classification/data/test/'
-dsets = imageandlabel(os.path.expanduser(data_dir),'img_test.csv', data_transforms)
+dsets = imageandlabel(args.rd,'img_test.csv', data_transforms)
 dset_loaders = torch.utils.data.DataLoader(dsets, batch_size=25, shuffle=True, num_workers=10)
 
 criterion = nn.CrossEntropyLoss()
@@ -41,7 +43,7 @@ i=0
 submission = {}
 model_ft = models.resnet50()
 model_ft.fc = nn.Linear(2048,3)
-model_ft.load_state_dict(torch.load('model_best.pth.tar')['model'])
+model_ft.load_state_dict(torch.load(args.cp)['model'])
 model_ft.cuda()
 
 for data in dset_loaders:
