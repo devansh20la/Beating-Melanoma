74ace0dad37b3bf8f1edebf36c3a14e08b4d6d77
devansh20la <db3484@nyu.edu>
2018-03-13 15:32:10 -0400

    classification

--- Classification/Train.py
+++ Classification/Train.py
@@ -11,23 +11,12 @@ import pandas as pd
 import random
 import shutil
 import argparse
-from model import network
 import pickle
 import numpy as np
 
-def save_checkpoint(state, is_best, filename,savetrainloss,savetraincorrects,savevalloss,savevalcorrects):
+def save_checkpoint(state, is_best, filename):
+
     torch.save(state, filename)
-
-    savetrainloss_name = open('trainloss.pkl','wb')
-    savetraincorrects_name = open('traincorrects.pkl','wb')
-    savevalloss_name = open('valloss.pkl','wb')
-    savevalcorrects_name = open('valcorrects.pkl','wb')
-
-    pickle.dump(savetrainloss,savetrainloss_name)
-    pickle.dump(savetraincorrects,savetraincorrects_name)
-    pickle.dump(savevalloss,savevalloss_name)
-    pickle.dump(savevalcorrects,savevalcorrects_name)
-
     if is_best:
         shutil.copyfile(filename, 'model_best.pth.tar')
         print("New Best Model Found")
@@ -46,9 +35,6 @@ def model_run(phase,model,inputs,labels,criterion,optimizer):
 
     optimizer.zero_grad()
     outputs = model(inputs)
-
-    if phase == 'train':
-        outputs = outputs[0]
 
     loss = criterion(outputs, labels)
     _, preds = torch.max(outputs.data, 1)
@@ -74,7 +60,7 @@ parser.add_argument('--lr','--learning_rate',type=float,default=0.001,help='init
 parser.add_argument('--lr_de','--lr_decay',type=int,default=30,help='learning rate decay epoch')
 parser.add_argument('--checkpoint',type=str,default='')
 parser.add_argument('--wd','--weightdecay',type=float,default=0)
-parser.add_argument('--rd','--root_dir',default='home/devansh/Documents/Melanoma/Classification/data')
+parser.add_argument('--rd','--root_dir',default='/home/devansh/Documents/Melanoma/Classification/data')
 
 args = parser.parse_args()
 
@@ -102,12 +88,12 @@ dset_loaders = {x: torch.utils.data.DataLoader(dsets[x], batch_size=25, num_work
                 for x in ['train', 'val']}
 
 print ("....Loading Model.....")
-model_ft = network()
+model_ft = models.resnet50(pretrained=True)
+model_ft.fc = nn.Linear(2048,3)
 
 if torch.cuda.is_available():
     model_ft = model_ft.cuda()
 
-print ("....Model loaded....")
 criterion = nn.CrossEntropyLoss()
 
 optimizer = optim.SGD(model_ft.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.wd)
@@ -120,18 +106,13 @@ if args.checkpoint:
     optimizer.load_state_dict(state['optimizer'])
     start_epoch = state['epoch']
     best_loss = state['best_loss']
+    savetrainloss = state['train_loss']
+    savetraincorrects = state['train_corrects']
+    savevalloss = state['val_loss']
+    savevalcorrects = state['val_corrects']
+
     print("checkpoint Loaded star_epoch = {0},best_loss= {1}".format(start_epoch,best_loss))
     del state
-    
-    savetrainloss_name = open('trainloss' + str(start_epoch) + '.pkl','rb')
-    savetraincorrects_name = open('traincorrects' + str(start_epoch) + '.pkl','rb')
-    savevalloss_name = open('valloss' + str(start_epoch) + '.pkl','rb')
-    savevalcorrects_name = open('valcorrects' + str(start_epoch) + '.pkl','rb')
-
-    savetrainloss = pickle.load(savetrainloss_name)
-    savetraincorrects = pickle.load(savetraincorrects_name)
-    savevalloss = pickle.load(savevalloss_name)
-    savevalcorrects = pickle.load(savevalcorrects_name)
 
     start_epoch+=1
 
@@ -184,6 +165,6 @@ for epoch in range(start_epoch,500):
     save_checkpoint({'epoch': epoch,
     'model': model_ft.state_dict(),
     'optimizer': optimizer.state_dict(),
-    'best_loss': best_loss},is_best,'checkpoint_ep%d.pth.tar'%(epoch),savetrainloss,savetraincorrects,savevalloss,savevalcorrects)
+    'best_loss': best_loss,'train_loss':savetrainloss,'train_corrects':savetraincorrects,'val_loss':savevalloss,'val_corrects':savevalcorrects},is_best,'checkpoints/checkpoint_ep%d.pth.tar'%(epoch))
     
     print ('Epoch = {0}, TrainingLoss = {1}, Train_corrects = {3},val Loss = {2}, val_corrects{4}'.format(epoch,trainloss,valloss,traincorrects,valcorrects))
